1f8480451c7014ba8f4baa68799d3f77994aac62
Niek van Aalst <nvanaalst441@student.novacollege.nl>
2024-11-19 11:33:14 +0100

    Feature: Post

--- www/index.php
+++ www/index.php
@@ -12,37 +12,37 @@
     <form action="process_form.php" method="post">
         <div class="form-group">
             <label for="name">voornaam</label>
-            <input type="text" name="" id="name">
+            <input type="text" name="name" id="name">
         </div>
         <div class="form-group">
             <label for="achter">Achternaam</label>
-            <input type="text" name="" id="achter">
+            <input type="text" name="achter" id="achter">
         </div>
         <div class="form-group">
             <label for="tusen">tussenvoegsel</label>
-            <input type="text" name="" id="tusen">
+            <input type="text" name="tussen" id="tusen">
         </div>
         <br>
 
         <div class="form-group">
             <label for="straat">straat</label>
-            <input type="text" name="" id="straat">
+            <input type="text" name="straat" id="straat">
         </div>
         <div class="form-group">
             <label for="huisnimmer">huisnimmer</label>
-            <input type="text" name="" id="huisnimmer">
+            <input type="text" name="huisnummer" id="huisnimmer">
         </div>
         <div class="form-group">
             <label for="postcode">postcode</label>
-            <input type="text" name="" id="postcode">
+            <input type="text" name="postcode" id="postcode">
         </div>
         <div class="form-group">
             <label for="tusen">plaats</label>
-            <input type="text" name="" id="tusen">
+            <input type="text" name="plaats" id="plaats">
         </div>
         <div class="form-group">
             <label for="land">land</label>
-            <input type="text" name="" id="land">
+            <input type="text" name="land" id="land">
         </div>
         <button type="submit">Sla op</button>
     </form>
---
+++ www/process_form.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+var_dump($_POST);
+
+$firstname = $_POST['name'];
+$lastname = $_POST['achter'];
+$tussen = $_POST['tussen'];
+
+$straat =  $_POST['straat'];
+$homenumber = $_POST['huisnummer'];
+$zipcode = $_POST['postcode'];
+$place = $_POST['plaats'];
+$country =$_POST['land'];
