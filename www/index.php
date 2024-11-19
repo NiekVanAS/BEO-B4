f8576f68b196b015f7d347f49537f577c59b0d1a
Niek van Aalst <nvanaalst441@student.novacollege.nl>
2024-11-19 10:59:42 +0100

    feathre added: form

---
+++ www/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="name">voornaam</label>
+            <input type="text" name="" id="name">
+        </div>
+        <div class="form-group">
+            <label for="achter">Achternaam</label>
+            <input type="text" name="" id="achter">
+        </div>
+        <div class="form-group">
+            <label for="tusen">tussenvoegsel</label>
+            <input type="text" name="" id="tusen">
+        </div>
+        <br>
+
+        <div class="form-group">
+            <label for="straat">straat</label>
+            <input type="text" name="" id="straat">
+        </div>
+        <div class="form-group">
+            <label for="huisnimmer">huisnimmer</label>
+            <input type="text" name="" id="huisnimmer">
+        </div>
+        <div class="form-group">
+            <label for="postcode">postcode</label>
+            <input type="text" name="" id="postcode">
+        </div>
+        <div class="form-group">
+            <label for="tusen">plaats</label>
+            <input type="text" name="" id="tusen">
+        </div>
+        <div class="form-group">
+            <label for="land">land</label>
+            <input type="text" name="" id="land">
+        </div>
+    </form>
+</body>
+
+</html>
